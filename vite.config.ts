b17d3f7466e99175c40f52397202fe5d9cf13ca7
kmckoy14 <keatonsport@gmail.com>
2025-03-20 16:34:36 -0700

    Created initial project component folders and files. Moved assets to the public directory, and moved global css to a styles directory. Configured vite config file so the DOM will render and update.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // This helps with file system watching in Linux
+    },
+    host: true, // Listen on all addresses
+    hmr: {
+      overlay: true, // Show errors as overlay
+    }
+  },
 })
