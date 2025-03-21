d67d0964e66a2cabe19f4b6751d6e99e8efed2c8
kmckoy14 <keatonsport@gmail.com>
2025-03-21 11:48:44 -0700

    Styled anchor tags

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ function App() {
   return (
     <>
       <div className="container">
-      <button className='btn'>View My Work</button> <br /><br />
-        
-      <button className='btn btn-outline'>hello world</button>
+        <a href="">Testing Link Styles</a>
+      
       </div>
       
       
